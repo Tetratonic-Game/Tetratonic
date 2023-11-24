df5bd78374c596d92fb8c6f2cf6583fe1bc3820a
Chamath Dharmasiri <chamathsd@gmail.com>
2023-11-24 11:04:16 -0600

    Refactor player colliders, attach to adversary and pickup

---
+++ Source/Tetratonic/PlayerCollider.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCollider.h"
+
+#include "PlayerPawn.h"
+
+// Sets default values for this component's properties
+UPlayerCollider::UPlayerCollider()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UPlayerCollider::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (UPrimitiveComponent* CollisionMesh = Cast<UPrimitiveComponent>(GetAttachParent()))
+	{
+		CollisionMesh->SetGenerateOverlapEvents(true);
+		CollisionMesh->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+		CollisionMesh->OnComponentBeginOverlap.AddDynamic(this, &UPlayerCollider::OnOverlapBegin);
+	}
+	else
+	{
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("PlayerCollider on Actor %s was not attached to a PrimitiveComponent."),
+			*GetOwner()->GetActorNameOrLabel());
+	}
+}
+
+
+void UPlayerCollider::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+
+void UPlayerCollider::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (APlayerPawn* Player = Cast<APlayerPawn>(OtherActor))
+	{
+		Player->SetScore(Player->GetScore() + ScoreModifier);
+	}
+}
